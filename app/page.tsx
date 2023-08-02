51e7842023423c590a2eadd3cb04eeff9079e1d8
bang0711 <bang07112004@gmail.com>
2023-08-02 17:27:41 +0700

    asd

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import { PrismaClient } from "@prisma/client";
+import Button from "@/components/Button";
 type Props = {};
 const prisma = new PrismaClient();
 async function HomePage({}: Props) {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Button />
+    </div>
+  );
 }
 
 export default HomePage;
